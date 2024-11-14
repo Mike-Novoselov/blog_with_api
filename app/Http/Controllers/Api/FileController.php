8299ac0750441e694bc11936761742c0657c9d1a
Mike-Novoselov <68411205+Mike-Novoselov@users.noreply.github.com>
2024-11-14 15:56:16 +0700

    added upload to database
    added migration model to file and database

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\File;
 class FileController extends Controller
 {
     //
     function upload(Request $request)
     {
-        $result =$request->file('file')->store('apiFile');
-        return ["result" => $result];
+        $file = new File;
+        $file -> path = $request->file('file')->store('apiFile'); // "path" - название столбца куда помещать строку пити файла
+        $result = $file -> save();
+        if ($result){
+            return ["result" => "File uploaded successfully"];
+        }else{
+            return ["result" => "File not uploaded"];
+        }
+
     }
 }
